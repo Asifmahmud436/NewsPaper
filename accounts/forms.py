aca37fc79497abad840f6f2366215610bc8be1b5
Zettex <safaandsafa4@gmail.com>
2024-09-23 00:30:47 +0600

    finished the beginners book

---
+++ accounts/forms.py
@@ -0,0 +1,12 @@
+from .models import CustomUser
+from django.contrib.auth.forms import UserChangeForm,UserCreationForm
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('username','email','age',)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm):
+        model = CustomUser
+        fields = ('username','email','age',)
